8d12b1caeb1f627c21c827ffb9e16d7b63fc57e1
systemQsa <82174398+systemQsa@users.noreply.github.com>
2021-12-14 16:48:26 +0200

    Update TrelloRestClient.java

--- src/main/java/rest/TrelloRestClient.java
+++ src/main/java/rest/TrelloRestClient.java
@@ -12,8 +12,8 @@ public class TrelloRestClient {
     public void sendCreateBoardRequest(){
         String url = "https://api.trello.com/1/boards/";
         Map<String,String> queryParams = new HashMap<>();
-        queryParams.put("key","80db5373ee6b8562f4c2bcc47da83124");
-        queryParams.put("token","0cec3d53776403b90e02cd1b492be32f40384a821c79ef48770e6dbc0be2d2ae");
+        queryParams.put("key","#####");
+        queryParams.put("token","######");
         queryParams.put("name"," B20 Board");
         Header header = new Header("Content-Type","application/json");
         //.accept()
