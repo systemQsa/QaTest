b37621000e1756df0e7653ad96d4225d64ff12a0
systemQsa <82174398+systemQsa@users.noreply.github.com>
2021-12-14 16:47:50 +0200

    Update TrelloRestClient.java

--- src/main/java/rest/TrelloRestClient.java
+++ src/main/java/rest/TrelloRestClient.java
@@ -7,16 +7,6 @@ import static io.restassured.RestAssured.given;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-key = ad7e2fb85d583015a2e883e97a0a6752
-token = 72815be243734e14c107ba025aa4c93d1d4948238a974b0dd0a3b36a7967b22c
-
-
-80db5373ee6b8562f4c2bcc47da83124
-0cec3d53776403b90e02cd1b492be32f40384a821c79ef48770e6dbc0be2d2ae
-
-https://trello.com/1/authorize?expiration=never&scope=read,write,account&response_type=token&name=ServerToken&key={80db5373ee6b8562f4c2bcc47da83124}
- */
 
 public class TrelloRestClient {
     public void sendCreateBoardRequest(){
